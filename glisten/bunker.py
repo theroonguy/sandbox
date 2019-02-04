eb93fdae951dffc97f7a0a72aaea4c398e8f7853
Xystikon <haroon@amanullah.org>
2019-02-04 12:08:47 -0600

    edited bunker.py --HA

--- glisten/bunker.py
+++ glisten/bunker.py
@@ -1,24 +1,46 @@
 import time
 import random
-
-def init():
-    global n, s, e, w, currentloc
     
-    currentloc='sleepingbag'
-    action()
-
-def action():
+def init():
+    global currentloc, n, e, s, w
+    
+    currentloc=0
+    n=0
+    e=0
+    s=0
+    w=0
+    
+    sector('Sleeping Bag', 'door', 'corner', 'wall', 'shelf', 'sleepingbag')
+    action(currentloc, n, e, s, w)
+    
+def action(currentloc, n, e, s, w):
     action == input('>> ')
     
-def sector(n, e, s, w, currentloc):
-    global n, e, s, w, currentloc
+    if action == 'n':
+        currentloc = n
+    if action == 'e':
+        currentloc = e
+    if action == 's':
+        currentloc = s
+    if action == 'w':
+        currentloc = w
+
+    print(currentloc)
+    action(currentloc, n, e, s, w)
+        
+def loc():
+    sector('Sleeping Bag', 'door', 'corner', 'wall', 'shelf', 'sleepingbag')
     
+def sector(name, n, e, s, w, currentloc):
+    #    global name, n, e, s, w, currentloc
+    
+    print(name+'\n')
     n=n
     e=e
     s=s
     w=w
     currentloc=currentloc
-
+    
 init()
 
 '''
