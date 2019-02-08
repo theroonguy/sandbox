a20dfbaaba50cac93461cecf865ad4cff8f6aecd
Xystikon <haroon@amanullah.org>
2019-02-08 11:57:18 -0600

    added descriptions --HA

--- glisten/bunker.py
+++ glisten/bunker.py
@@ -2,13 +2,15 @@ import time
 import random
     
 def init():
-    global currentloc, n, e, s, w
+    global currentloc, n, e, s, w, descs
     
     currentloc=0
     n=0
     e=0
     s=0
     w=0
+
+    descs = []
     
     loc('sleepingbag')
     
@@ -16,30 +18,52 @@ def action(cl, n, e, s, w):
     action = input('>> ')
     
     if action == 'n':
-        cl = n
+        if n != 'wall':
+            cl = n
     elif action == 'e':
-        cl = e
+        if e != 'wall':
+            cl = e
     elif action == 's':
-        cl = s
+        if s != 'wall':
+            cl = s
     elif action == 'w':
-        cl = w
-    
+        if w != 'wall':
+            cl = w
+
+    elif action == 'l':
+        loc(cl)
+        
+    elif action == 'quit':
+        quit()
+
+    else:
+        print('Sorry, I didn\'t understand.')
+
+        
     loc(cl)
     
 def loc(loc):
 
     if loc == 'sleepingbag':
-        sector('Sleeping Bag', 'door', 'corner', 'wall', 'wall', 'sleepingbag')        
+        sector('Sleeping Bag', 'door', 'corner', 'wall', 'wall', 'sleepingbag','a sleeping bag lol')        
     elif loc == 'door':
-        sector('Door', 'wall', 'shelf', 'sleepingbag', 'wall', 'door')
+        sector('Door', 'wall', 'shelf', 'sleepingbag', 'wall', 'door', 'a random door')
     elif loc == 'shelf':
-        sector('A Shelf', 'wall', 'wall', 'corner', 'door', 'shelf')
+        sector('A Shelf', 'wall', 'wall', 'corner', 'door', 'shelf','a random shelf thing')
     elif loc == 'corner':
-        sector('Corner', 'shelf', 'wall', 'wall', 'sleepingbag', 'corner')
+        sector('Corner', 'shelf', 'wall', 'wall', 'sleepingbag', 'corner','a corner with nothing')
     
-def sector(name, n, e, s, w, currentloc):
+def sector(name, n, e, s, w, currentloc, desc):
+    global descs
+
     print(name+'\n')
-    
+
+    if desc in descs:
+        print('')
+    else:
+        descs.append(desc)
+        print(desc)
+
     action(currentloc, n, e, s, w)
     
 init()
