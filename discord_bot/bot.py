fe202c80005cc31beb70f168cd64a7814b776ce1
Haroon <haroon@amanullah.org>
2019-02-14 19:24:40 -0600

    polished weather for bot.py

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -10,7 +10,11 @@ data = res.json()
 temp = data['main']['temp']
 wind_speed = data['wind']['speed']
 description = data['weather'][0]['description']
-    
+
+ftemp = 'Temperature : {} Degrees Kelvin'.format(temp)
+fwind = 'Wind Speed : {} m/s'.format(wind_speed)
+fdesc = 'Description : {}'.format(description)
+
 TOKEN = 'NTQzOTUzNDM2NTUxNDEzNzcw.D0EFWQ.koaPnK_GT59KJtfKqP4Xis2CqKI'
 
 client = commands.Bot(command_prefix = 's/')
@@ -26,34 +30,19 @@ async def ping():
 
 @client.command()
 async def weather():
-
-<<<<<<< HEAD
-    await client.say('Temperature : {} degree celcius'.format(temp))
-=======
-    temp = data['main']['temp']
-    wind_speed = data['wind']['speed']
-
-    latitude = data['coord']['lat']
-    longitude = data['coord']['lon']
-
-    description = data['weather'][0]['description']
-
-    await client.say('Temperature : {} degree fahrenheit'.format(temp))
->>>>>>> bdb3e354acd1fd29e83f584904c98999b6c915f5
-    await client.say('Wind Speed : {} m/s'.format(wind_speed))
-    await client.say('Description : {}'.format(description))
-
+    await client.say(ftemp+'\n'+fwind+'\n'+fdesc)
+    
 @client.command()
 async def wind():
-    await client.say('Wind Speed: {} m/s'.format(wind_speed))
+    await client.say(fwind)
 
 @client.command()
 async def desc():
-    await client.say('Description: {}'.format(description))
+    await client.say(fdesc)
 
 @client.command()
 async def temp():
-    await client.say('Temperature: {}'.format(temp))
+    await client.say(ftemp)
 
 @client.command()
 async def echo(*args):
