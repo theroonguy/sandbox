8f40361b20a77a2ff85a525c49c5b1f7ccc52575
Haroon <haroon@amanullah.org>
2019-02-14 19:07:56 -0600

    FINISHED WEATHER

--- discord_bot/bot.py
+++ discord_bot/bot.py
@@ -1,14 +1,18 @@
 import discord
 from discord.ext import commands
-import pyowm
+import pprint
+import requests
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q=Fairfax,us&appid=ac7c75b9937a495021393024d0a90c44'
+
+res = requests.get(url)
+
+data = res.json()
 
 TOKEN = 'NTQzOTUzNDM2NTUxNDEzNzcw.D0EFWQ.koaPnK_GT59KJtfKqP4Xis2CqKI'
 
 client = commands.Bot(command_prefix = 's/')
 
-owm = pwowm.OWM()
-observation = owm.weather_at_place('Vienna,VA')
-w = observation.get_weather()
 
 @client.event
 async def on_ready():
@@ -18,13 +22,24 @@ async def on_ready():
 async def ping():
     await client.say('Pong')
 
+
 @client.command()
 async def weather():
-    wind=w.get_wind()
-    humidity=w.get_humidity()
-    client.say('Wind: '+wind)
-    client.say('Humidity: '+humidity)
 
+    temp = data['main']['temp']
+    wind_speed = data['wind']['speed']
+
+    latitude = data['coord']['lat']
+    longitude = data['coord']['lon']
+
+    description = data['weather'][0]['description']
+
+    await client.say('Temperature : {} degree celcius'.format(temp))
+    await client.say('Wind Speed : {} m/s'.format(wind_speed))
+    await client.say('Latitude : {}'.format(latitude))
+    await client.say('Longitude : {}'.format(longitude))
+    await client.say('Description : {}'.format(description))
+    
 @client.command()
 async def echo(*args):
     output = ''
