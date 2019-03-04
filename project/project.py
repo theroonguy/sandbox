fa19389e3c56e89e9a0c489ddcd706e8e0053f9e
Xystikon <haroon@amanullah.org>
2019-03-04 08:11:13 -0600

    modifed

--- project/project.py
+++ project/project.py
@@ -6,13 +6,24 @@ topframe=Frame(root)
 topframe.pack(fill=X)
 bottomframe=Frame(root)
 bottomframe.pack(side=BOTTOM, fill=Y)
+middleframe=Frame(root)
+middleframe.pack()
 
 button1 = Button(topframe, text='This is a button', bg='purple', fg='purple')
 button1.pack(fill=X)
 label1 = Label(root, text='This is a label', fg='purple')
 label1.pack(fill=Y, side=LEFT)
 
+name = Label(middleframe, text='NAME')
+password = Label(middleframe, text='PASSWORD')
+entry1 = Entry(middleframe)
+entry2 = Entry(middleframe)
 
+name.grid(row=0)
+password.grid(row=1)
+
+entry1.grid(row=0, column=1)
+entry2.grid(row=1, column=1)
 
 '''
 w = Canvas(master, width=200, height=100)
