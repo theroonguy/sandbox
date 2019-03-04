17d0b606c6fcf8a22f1e53e26d541768b312bdd4
Xystikon <haroon@amanullah.org>
2019-03-04 08:03:41 -0600

    modifies

--- project/project.py
+++ project/project.py
@@ -1,7 +1,20 @@
 from tkinter import *
 
-master=Tk()
+root=Tk()
 
+topframe=Frame(root)
+topframe.pack(fill=X)
+bottomframe=Frame(root)
+bottomframe.pack(side=BOTTOM, fill=Y)
+
+button1 = Button(topframe, text='This is a button', bg='purple', fg='purple')
+button1.pack(fill=X)
+label1 = Label(root, text='This is a label', fg='purple')
+label1.pack(fill=Y, side=LEFT)
+
+
+
+'''
 w = Canvas(master, width=200, height=100)
 w.pack()
 
@@ -9,6 +22,8 @@ w.create_line(0, 0, 200, 100)
 w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
 
 w.create_rectangle(50, 25, 150, 75, fill="blue")
+'''
+
 
 mainloop()
 
