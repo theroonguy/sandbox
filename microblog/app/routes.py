503f241a84c0abe45d885c1bb711850399c6d811
Haroon <haroon@amanullah.org>
2019-07-08 16:21:51 -0400

    nothing really...

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -5,4 +5,15 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Haroon'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Averngers movie was so cool!'
+        }
+    ]
+    
+    return render_template('index.html', title='Home', user=user, posts=posts)
