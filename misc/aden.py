aa70afe6a36c60f0b81fd7e6bcf9ce04e564b2e8
Haroon <haroon@amanullah.org>
2019-11-28 12:37:54 -0500

    new commit

---
+++ misc/aden.py
@@ -0,0 +1,6 @@
+action = input("Aden is here.")
+
+if action == 'ok':
+    print('good')
+else:
+    print('ok byeyou pressed something else')
---
+++ misc/test.py
@@ -0,0 +1,8 @@
+
+names = []
+
+name = input('Hello! What is your name? ')
+
+names.append(name)
+
+print(names)
